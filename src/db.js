addc6d8000065f80b2d4f8437be56cdf50c117c4
Antti Sartjärvi <antti.sartjarvi@gmail.com>
2018-09-10 12:07:09 +0300

    Use pg pools instead of clients

--- src/db.js
+++ src/db.js
@@ -1,28 +1,30 @@
-const { Client } = require('pg');
+const { Pool } = require('pg');
 const config = require('./config');
+
+const pool = new Pool(config.db);
+
+const createQuery = async (text, values) => {
+  const query = await pool.query(text, values)
+    .then(res => res.rows)
+    .catch(e => console.log(e.stack));
+
+  return query;
+};
 
 module.exports = {
   favorite: {
     add: async (user, place) => {
-      const client = new Client(config.db);
-      client.connect();
-
       const text = 'INSERT INTO favorites(user_id, place) VALUES($1, $2) RETURNING *';
+      const query = await createQuery(text, [user, place]);
 
-      return client.query(text, [user, place])
-        .then(res => res.rows[0])
-        .catch(e => console.error(e.stack));
+      return query;
     },
 
     remove: async (user, place) => {
       const text = 'DELETE FROM favorites WHERE user_id = $1 AND place = $2 RETURNING *';
+      const query = await createQuery(text, [user, place]);
 
-      const client = new Client(config.db);
-      client.connect();
-
-      return client.query(text, [user, place])
-        .then(res => res.rows)
-        .catch(e => console.error(e.stack));
+      return query;
     },
 
     find: async (opt) => {
@@ -30,12 +32,7 @@ module.exports = {
       const text = `SELECT * FROM favorites WHERE ${keyStr}`;
       const values = Object.values(opt);
 
-      const client = new Client(config.db);
-      client.connect();
-
-      const result = await client.query(text, values)
-        .then(res => res.rows)
-        .catch(e => console.error(e.stack));
+      const result = await createQuery(text, values);
 
       return result.map(f => f.place);
     },
